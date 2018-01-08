6a330cd5767e14cad95e284407957a6c1e41d08f
Mahesh <mp1995singh@gmail.com>
2018-01-08 14:15:41 +0530

    ..

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 $(function(){
+	$('#home').trigger('focus');
 	$('#home').click(function(){
 		$('.mg-section').hide();
 		$('.landing-section').fadeIn();
-		// $('.mg-section').addClass('index-0');
-		// $('.landing-section').removeClass('index-0');
 	});
 	$('#about').click(function(){
 		$('.mg-section').hide();
 		$('.about-content').fadeIn();
-		// $('.mg-section').addClass('index-0');
-		// $('.about-content').removeClass('index-0');
 	});
 });
 
