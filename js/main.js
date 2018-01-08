bf3e76826fff031ad16a046321fc4cd980915897
Mahesh <mp1995singh@gmail.com>
2018-01-08 14:08:20 +0530

    ..

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(function(){
 	$('#home').click(function(){
-		$('.mg-section').addClass('index-0');
-		$('.landing-section').removeClass('index-0');
-		$('body').find('animated').removeClass('animated fadeIn');
-		$('.desc').addClass('animated fadeInUp');
+		$('.mg-section').hide();
+		$('.landing-section').fadeIn();
+		// $('.mg-section').addClass('index-0');
+		// $('.landing-section').removeClass('index-0');
 	});
 	$('#about').click(function(){
-		$('.mg-section').addClass('index-0');
-		$('.about-content').removeClass('index-0');
-		$('body').find('animated').removeClass('animated fadeIn');
-		$('.mid-content-div').addClass('animated fadeIn');
+		$('.mg-section').hide();
+		$('.about-content').fadeIn();
+		// $('.mg-section').addClass('index-0');
+		// $('.about-content').removeClass('index-0');
 	});
 });
 
