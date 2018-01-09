1aa581ffe9ef3a7bfa2f44d00e9b3f1317782561
Mahesh <mp1995singh@gmail.com>
2018-01-09 21:20:11 +0530

    ..

--- js/main.js
+++ js/main.js
@@ -2,12 +2,19 @@ $(function(){
 	$('#home').trigger('focus');
 	$('#home').click(function(){
 		$('.mg-section').fadeOut();
+		$('.m1').removeClass('socialMax');
 		$('.landing-section').fadeIn();
 	});
 	$('#about').click(function(){
 		$('.mg-section').fadeOut();
 		$('.mg-section').hide();
+		$('.m1').removeClass('socialMax');
 		$('.about-content').fadeIn();
+	});
+	$('#contact').click(function(){
+		var side = $('.m1');
+		console.log(side);
+		side.addClass('socialMax');
 	});
 });
 
