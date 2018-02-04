997cbcae859fc039b4130b8871ac87a58bfc0a8a
Mahesh <mp1995singh@gmail.com>
2018-02-04 14:57:30 +0530

    .

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
 $(function(){
-	$('#home').trigger('focus');
+	$('.mp-main').load('landing.html');
 	$('#home').click(function(){
-		$('.mg-section').fadeOut();
+		// $('.mg-section').fadeOut();
 		$('.m1').removeClass('socialMax');
-		$('.landing-section').fadeIn();
+		// $('.landing-section').fadeIn();
+		$('.mp-main').load('landing.html');
 	});
 	$('#about').click(function(){
-		$('.mg-section').fadeOut();
-		$('.mg-section').hide();
+		// $('.mg-section').fadeOut();
+		// $('.mg-section').hide();
 		$('.m1').removeClass('socialMax');
-		$('.about-content').fadeIn();
+		// $('.about-content').fadeIn();
+		$('.mp-main').load('about.html');
 	});
 	$('#contact').click(function(){
 		var side = $('.m1');
-		console.log(side);
 		side.addClass('socialMax');
 	});
 	$('#portfolio').click(function(){
-		$('.mg-section').fadeOut();
-		$('.mg-section').hide();
+		// $('.mg-section').fadeOut();
+		// $('.mg-section').hide();
 		$('.m1').removeClass('socialMax');
-		$('.portfolio').fadeIn();
+		// $('.portfolio').fadeIn();
+		$('.mp-main').load('portfolio.html');
 	});
 	$('.work-btn').click(function(){
-		$('#portfolio').trigger('click');
+		// $('#portfolio').trigger('click');
+		$('.mp-main').load('portfolio.html');
 	})
 });
 
