25e5dd36b7db69534ce26e3a4978d3a1a21faa49
Mahesh <mp1995singh@gmail.com>
2018-03-06 21:36:10 +0530

    new

--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
 $(function(){
+	$('#home>a').addClass('active-link');
 	$('.mp-main').load('landing.html');
 	$('#home').click(function(){
-		// $('.mg-section').fadeOut();
 		$('.m1').removeClass('socialMax');
-		// $('.landing-section').fadeIn();
+		$('.li>a').removeClass('active-link');
+		$('#home>a').addClass('active-link');
 		$('.mp-main').load('landing.html');
 	});
 	$('#about').click(function(){
-		// $('.mg-section').fadeOut();
-		// $('.mg-section').hide();
 		$('.m1').removeClass('socialMax');
-		// $('.about-content').fadeIn();
+		$('.li>a').removeClass('active-link');
+		$('#about>a').addClass('active-link');
 		$('.mp-main').load('about.html');
 	});
 	$('#contact').click(function(){
 		var side = $('.m1');
+		$('.li>a').removeClass('active-link');
+		$('#contact>a').addClass('active-link');
 		side.addClass('socialMax');
 	});
 	$('#portfolio').click(function(){
-		// $('.mg-section').fadeOut();
-		// $('.mg-section').hide();
 		$('.m1').removeClass('socialMax');
-		// $('.portfolio').fadeIn();
+		$('.li>a').removeClass('active-link');
+		$('#portfolio>a').addClass('active-link');
 		$('.mp-main').load('portfolio.html');
 	});
-	$('.work-btn').click(function(){
-		// $('#portfolio').trigger('click');
-		$('.mp-main').load('portfolio.html');
-	})
+
+	$('.nav-btn').click(function(){
+		console.log("this running");
+		$('.nav-btn-lines').eq(0).toggleClass('nav-btn-lines-rt1');
+		$('.nav-btn-lines').eq(1).toggleClass('nav-btn-lines-rt2');
+		$('.header-logo').toggleClass('header-logo-max');
+		$('.m5').toggleClass('m5-max');
+		
+	});
+	$('.m5').find('.m5-a').click(function(){
+		$('.nav-btn').trigger('click');
+	});
 });
 
